21ea0deb0b3212dba08601aa5935d2f30f28fd84
anhtrannhat <anh.tran@bluecomgroup.com>
2018-10-05 17:43:59 +0700

    finish the file listener

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 var config = {};
-config.host="https://demo-apacestore-pandora.demandware.net/on/demandware.servlet/webdav/Sites/Logs";
-config.username="phong.nguyen";
-config.password="Demandware@2345678901@#$";
+config.webdav={};
+config.filemanager={};
+
+config.webdav.host="https://demo-apacestore-pandora.demandware.net/on/demandware.servlet/webdav/Sites/Logs";
+config.webdav.username="phong.nguyen";
+config.webdav.password="Demandware@2345678901@#";
+config.filemanager.host="localhost";
+config.filemanager.db="log_record";
+config.filemanager.username="admin";
+config.filemanager.password="nTR2BUDse2kfHXkP";
+config.filemanager.db_type="mysql";
 module.exports = config;
 
--- listener/WebDavFileManager.js
+++ listener/WebDavFileManager.js
@@ -1,18 +1,31 @@
+const Sequelize = require('sequelize');
+var config = require("../config");
 function WebDavFileManager() {
     this.fileArrays = [];
 };
 WebDavFileManager.prototype.readDirInfo = function(fileListInfo)  {
+    var jsonData = JSON.parse(fileListInfo);
     //change info last modification time , from string to Date
-    for(var i = 0 ; i < fileListInfo.length ; i++) {
-        var fileInfo = fileListInfo[i];
+    for(var i = 0 ; i < jsonData.length ; i++) {
+        var fileInfo = jsonData[i];
         var parsedDate = Date.parse(fileInfo.lastmod);
         fileInfo.timeStamp = parsedDate;
-        
-        //this.fileArrays.push(fileInfo);
+        this.fileArrays.push(fileInfo);
     }
+    
 }
-WebDavFileManager.prototype.returnFileArray = function() {
-    return this.fileArrays;
+WebDavFileManager.prototype.saveIntoFileManager = function() {
+    const sequelize = new Sequelize(config.filemanager.db, config.filemanager.username, config.filemanager.password, {
+        host: config.filemanager.host,
+        dialect: config.filemanager.db_type,
+        operatorsAliases: false,
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000
+        }
+    });
 }
 module.exports = WebDavFileManager;
 
